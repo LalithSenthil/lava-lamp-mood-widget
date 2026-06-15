40c476341ed90a852609c23b6ee9aa0c1e507f41
Lalith <2018241@apps.nsd.org>
2026-06-15 15:36:03 -0700

    wip - not working with rukman

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,12 @@
 module lava.lamp.mood.widget {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
+
+    requires java.desktop; // for java.awt (TimeColorProvider)
+    requires java.prefs;   // for Preferences API
+
+    opens com.example.lavalamp.ui to javafx.fxml;
 
     exports com.example.lavalamp;
     exports com.example.lavalamp.core;
